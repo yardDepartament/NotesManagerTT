d4a6852b198675534c0368e5b261ad3341d216ec
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-10-21 09:52:58 +0300

    add serializers.py, for Notes

---
+++ api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Заметок.
+
+    """
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at']
