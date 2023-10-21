8c416c35f0c8930e4a6bb2476978617a642c6889
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-10-21 11:11:16 +0300

    add docstring in model's method

--- api/models.py
+++ api/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-from django.db import models
-
-
 class Note(models.Model):
     """
     Модель для заметок.
@@ -11,25 +8,49 @@ class Note(models.Model):
     Атрибуты:
         title (str): Заголовок заметки.
         content (str): Содержимое заметки.
-        created_at (datetime): Дата время создания, создаётся автоматически при добавлении обьекта.
+        created_at (datetime): Дата время создания, создаётся автоматически при добавлении объекта.
 
     Methods:
         __str__: возвращает заголовок заметки.
-        update_content: Обновляет содежримое заметки.
+        update_content: Обновляет содержимое заметки.
         delete_note: Удаляет заметку.
 
-
     """
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Возвращает заголовок заметки.
+
+        Returns:
+            str: Заголовок заметки.
+
+        """
         return self.title
 
     def update_content(self, new_content):
+        """
+        Обновляет содержимое заметки.
+
+        Args:
+            new_content (str): Новое содержимое заметки.
+
+        Returns:
+            None
+
+        """
         self.content = new_content
         self.save()
 
     def delete_note(self):
+        """
+        Удаляет заметку.
+
+        Returns:
+            None
+
+        """
         self.delete()
