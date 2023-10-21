44f45a424c69451acab811be9efa18b19a77d49b
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-10-21 09:54:32 +0300

    make views for Notes and any operations with it

--- api/views.py
+++ api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+class NoteListCreateView(generics.ListCreateAPIView):
+    """
+    View for listing and creating notes.
+
+    """
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+
+class NoteEditView(generics.UpdateAPIView):
+    """
+    View for editing an existing note.
+
+    """
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+
+class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for retrieving, updating and deleting a note.
+
+    """
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
