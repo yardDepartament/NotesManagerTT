ae27c238862f11397d9632a012d63635f183fd80
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-10-21 11:07:49 +0300

    add some comments for urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,13 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import NoteViewSet
 
+# Создаем экземпляр DefaultRouter
 router = DefaultRouter()
+
+# Регистрируем NoteViewSet с путем 'notes/' и базовым именем 'note'
 router.register(r'notes', NoteViewSet, basename='note')
 
+# Определяем urlpatterns, включая пути созданные маршрутизатором
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  # Включаем пути из маршрутизатора
 ]
