b24621be80886331856fa05d5b29d76a3a68505e
YARD DEPARTAMENT <ivankochettov@gmail.com>
2023-10-21 11:23:22 +0300

    add coments for projects urls.py

--- NotesManagerTT/urls.py
+++ NotesManagerTT/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-import api
-
 
 urlpatterns = [
+    # Путь к админ-панели Django
     path('admin/', admin.site.urls),
+    
+    # Путь к API версии 1
     path('api/v1/', include('api.urls')),
 ]
